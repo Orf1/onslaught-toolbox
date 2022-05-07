6e182d940976df062a574520592f2b7771f5f24a
Orf1 <orfeas126@gmail.com>
2022-05-07 12:04:30 -0700

    Finished waves generation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import com.github.kinquirer.KInquirer
 import com.github.kinquirer.components.promptInput
+import com.github.kinquirer.components.promptInputNumber
 import com.github.kinquirer.components.promptList
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonArray
@@ -83,7 +84,7 @@ fun buildSelector(): JsonObject {
     val selector = JsonObject()
 
     selector.add("gamestages", buildGamestages())
-    selector.addProperty("weight", KInquirer.promptInput(message = "What should the siege weight be?"))
+    selector.addProperty("weight", KInquirer.promptInputNumber(message = "What should the siege weight be?"))
     selector.add("dimension", buildDimension())
 
     return selector
@@ -121,7 +122,7 @@ fun buildCommands(): JsonObject {
                     val stagedElement = JsonObject()
                     stagedElement.addProperty(
                         "complete",
-                        KInquirer.promptInput(message = "What would you like complete value to be?")
+                        KInquirer.promptInputNumber(message = "What would you like complete value to be?")
                     )
                     stagedElement.add(
                         "commands",
@@ -181,8 +182,132 @@ fun buildMessages(): JsonObject {
     return messages
 }
 
-fun buildWaves(): JsonObject {
-    val waves = JsonObject()
+/*
+    "waves": [
+      {
+        "delayTicks": [ 0 ],
+        "groups": [
+          {
+            "mobs": [
+              {
+                "id": "zombiesiege.grunt",
+                "count": [ 14, 16 ],
+                "spawn": {
+                  "type": "ground",
+                  "light": [
+                    0,
+                    7
+                  ],
+                  "rangeXZ": [
+                    16,
+                    32
+                  ],
+                  "rangeY": 8,
+                  "stepRadius": 4,
+                  "sampleDistance": 2
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+ */
+fun buildWaves(): JsonArray {
+    val waves = JsonArray()
+
+    fun buildSpawn(): JsonObject {
+        val spawn = JsonObject()
+
+        spawn.addProperty(
+            "type",
+            KInquirer.promptList(
+                message = "What should the spawn type be?",
+                choices = listOf("ground", "air", "beneath")
+            )
+        )
+        spawn.add("light", promptArray("What should the light range be? Format: min,max"))
+        spawn.add("light", promptArray("What should the XZ range be? Format: min,max"))
+        spawn.addProperty("rangeY", KInquirer.promptInputNumber(message = "What should the rangeY be?"))
+        spawn.addProperty("stepRadius", KInquirer.promptInputNumber(message = "What should the step radius be?"))
+        spawn.addProperty(
+            "sampleDistance",
+            KInquirer.promptInputNumber(message = "What should the sample distance be?")
+        )
+
+        return spawn
+    }
+
+    fun buildMobs(): JsonArray {
+        val mobs = JsonArray()
+        while (true) {
+            when (KInquirer.promptList(
+                message = "Would you like to add a new mob to the group?",
+                choices = listOf("Add Mob", "Done")
+            )) {
+                "Add Mob" -> {
+                    val mob = JsonObject()
+
+                    mob.addProperty("id", KInquirer.promptInput("What is the mob id?"))
+                    mob.add("count", promptArray("What should the count be? (You can use commas for multiple numbers)"))
+                    mob.add("spawn", buildSpawn())
+                    mobs.add(mob)
+                }
+                "Done" -> {
+                    break
+                }
+            }
+
+        }
+        return mobs
+    }
+
+    fun buildGroups(): JsonArray {
+        val groups = JsonArray()
+
+        while (true) {
+            when (KInquirer.promptList(
+                message = "Would you like to add a new group to the wave?",
+                choices = listOf("Add Group", "Done")
+            )) {
+                "Add Group" -> {
+                    val group = JsonObject()
+                    group.addProperty("weight", KInquirer.promptInputNumber("What should the group weight be?"))
+                    group.addProperty(
+                        "forceSpawn",
+                        KInquirer.promptList(
+                            message = "Should forceSpawn be enabled?",
+                            choices = listOf("true", "false")
+                        )
+                    )
+                    group.add("mobs", buildMobs())
+                    groups.add(group)
+                }
+                "Done" -> {
+                    break
+                }
+            }
+        }
+        return groups
+    }
+
+    while (true) {
+        when (KInquirer.promptList(
+            message = "Would you like to add a new wave?",
+            choices = listOf("New Wave", "Done")
+        )) {
+            "New Wave" -> {
+                val wave = JsonObject()
+                wave.add("delayTicks", promptArray("How many delay ticks should this wave have?"))
+                wave.add("groups", buildGroups())
+                waves.add(wave)
+            }
+            "Done" -> {
+                break
+            }
+        }
+
+    }
 
     return waves
 }
@@ -198,7 +323,11 @@ fun promptArray(prompt: String): JsonArray {
     val jsonArray = JsonArray()
 
     for (s in raw.split(",")) {
-        jsonArray.add(s)
+        try {
+            jsonArray.add(s.toInt())
+        } catch (e: java.lang.NumberFormatException) {
+            jsonArray.add(s)
+        }
     }
 
     return jsonArray
